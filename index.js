34a731b045099688da558ad7b25d8402e589fa90
elexis <eebrown422@gmail.com>
2022-02-22 00:25:44 -0500

    push

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = 'elexis';
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('elexis');
+greet('michael');
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['elexis', 'michael', 'connor', 'cal'];
+const ages = [18, 21, 13, 13]
+console.log(people);
+
+module.exports = {
+    people, ages
+}
